0190a6a397fc83f6069d0debb2acf76e78984df5
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-31 01:57:49 +0200

    finished emitters refactor

--- models/emitters.js
+++ models/emitters.js
@@ -1,7 +1,7 @@
 const users = require('./users');
 const gamelogics = require('./gamelogics');
 
-let checkuserForEmit = socket =>
+const checkUserNameAvailability = socket =>
   socket.on(
     'username',
     newUsername =>
@@ -10,58 +10,58 @@ let checkuserForEmit = socket =>
         : socket.broadcast.emit('usernameOK', newUsername)
   );
 
-const waitingForGame = (socket, io) => {
-  socket.on('startGame', newUsername => {
-    console.log(newUsername);
-    let socketid = users.findsocketID(newUsername);
-    let myUsername = users.findUserName(socket.id);
-    let CurrentGame = new gamelogics(socket.id, socketid); //saves first game
-    let game = CurrentGame.objOfPlayers; //first player that started the game is first playing player0
-    users.currentGame.push(CurrentGame);
-    CurrentGame.currentPlayer = game[0].id;
-    io.to(game[0].id).emit('connetionBeforeGame', game[0].board);
-    io.to(game[1].id).emit('connetionBeforeGame', game[1].board, true, newUsername);
-    io.to(game[0].id).emit('2playersPlaying', myUsername, newUsername);
-    socket.broadcast.emit('2playersPlaying', myUsername, newUsername);
-  });
-
-  socket.on('checkifHit', cell => {
-    //User Connected
-    let CurrentGame = users.findCurrentGame(socket.id);
-    let answer = CurrentGame.checkGame(socket.id, cell - 100);
-    if (CurrentGame.ifPlayerAllowed(socket.id, answer)) {
-      //checks whos turn it is and allows if its right player turn.
-      let opponent = CurrentGame.FindOtherPlayer(socket.id, answer);
-      //let answer = CurrentGame.checkGame(socket.id, cell - 100);
-      if (!CurrentGame.gameEnds(answer, socket.id)) {
-        socket.emit('answerIfHit', { answer, cell });
-        io.to(opponent).emit('answerIfHitFromOtherPlayer', { answer: answer, cell: cell, opponent: true });
-      } else {
-        socket.emit('answerIfHit', { answer, cell });
-        io.to(opponent).emit('answerIfHitFromOtherPlayer', { answer: answer, cell: cell, opponent: true });
-        socket.emit('endGame', users.findUserName(socket.id), users.findUserName(opponent));
-        socket.broadcast.emit('endGame', users.findUserName(socket.id), users.findUserName(opponent), true);
-        io.to(opponent).emit('endGame', users.findUserName(socket.id), users.findUserName(opponent), false);
-      }
-    }
-  });
+const startGame = (socket, io, newUsername) => {
+  const myUsername = users.findUserName(socket.id);
+  const CurrentGame = new gamelogics(socket.id, users.findsocketID(newUsername));
+  const game = CurrentGame.objOfPlayers;
+  users.currentGame.push(CurrentGame);
+  CurrentGame.currentPlayer = game[0].id;
+  io.to(game[0].id).emit('connetionBeforeGame', game[0].board);
+  io.to(game[1].id).emit('connetionBeforeGame', game[1].board, true, newUsername);
+  io.to(game[0].id).emit('2playersPlaying', myUsername, newUsername);
+  socket.broadcast.emit('2playersPlaying', myUsername, newUsername);
 };
 
-const dissconnect = socket =>
+const continueGame = (socket, io, answer, cell, opponent) => {
+  socket.emit('answerIfHit', { answer, cell });
+  io.to(opponent).emit('answerIfHitFromOtherPlayer', { answer: answer, cell: cell, opponent: true });
+};
+
+const endGame = (socket, io, answer, cell, opponent) => {
+  socket.emit('answerIfHit', { answer, cell });
+  io.to(opponent).emit('answerIfHitFromOtherPlayer', { answer: answer, cell: cell, opponent: true });
+  socket.emit('endGame', users.findUserName(socket.id), users.findUserName(opponent));
+  socket.broadcast.emit('endGame', users.findUserName(socket.id), users.findUserName(opponent), true);
+  io.to(opponent).emit('endGame', users.findUserName(socket.id), users.findUserName(opponent), false);
+};
+
+const checkIfHit = (socket, io, cell) => {
+  const CurrentGame = users.findCurrentGame(socket.id);
+  let answer = CurrentGame.checkGame(socket.id, cell - 100);
+  if (!CurrentGame.ifPlayerAllowed(socket.id, answer)) {
+    return;
+  }
+  let opponent = CurrentGame.FindOtherPlayer(socket.id, answer);
+  CurrentGame.gameEnds(answer, socket.id)
+    ? endGame(socket, io, answer, cell, opponent)
+    : continueGame(socket, io, answer, cell, opponent);
+};
+
+const startConnection = (socket, io, dataBeforeConnection) => {
+  const usersInServer = users.getUsers();
+  socket.emit('connection Established', usersInServer);
+  checkUserNameAvailability(socket);
+  socket.on('startGame', newUsername => startGame(socket, io, newUsername));
+  socket.on('checkifHit', cell => checkIfHit(socket, io, cell));
+};
+
+const emiters = (socket, io) => {
+  socket.on('startConnection', dataBeforeConnection => startConnection(socket, io, dataBeforeConnection));
   socket.on('disconnect', () => {
     const username = users.findUserName(socket.id);
     users.removeUser(username);
     socket.broadcast.emit('disconnectedUser', username);
   });
-
-const emiters = (socket, io) => {
-  socket.on('startConnection', dataBeforeConnection => {
-    const usersInServer = users.getUsers();
-    socket.emit('connection Established', usersInServer);
-    checkuserForEmit(socket);
-    waitingForGame(socket, io);
-  });
-  dissconnect(socket);
 };
 
 module.exports = emiters;
